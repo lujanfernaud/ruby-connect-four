a513f095e3f0b364d82ea10aac6133d21a4a9ee1
lujanfernaud <lujanfernaud@gmail.com>
2017-05-23 17:25:55 +0100

    Add Computer#throw feature

--- lib/connect_four.rb
+++ lib/connect_four.rb
@@ -1,3 +1,5 @@
+require "pry"
+
 class Player
   attr_accessor :name, :mark
   attr_reader   :board
@@ -15,13 +17,207 @@ class Player
 end
 
 class Computer < Player
-  attr_accessor :name, :mark
-  attr_reader   :board
+  attr_accessor :name,  :mark
+  attr_reader   :board, :human_mark
 
-  def initialize(mark:, board: [])
-    @name  = "Computer"
-    @mark  = mark
-    @board = board
+  def initialize(mark:, human_mark:, board: [])
+    @name       = "Computer"
+    @mark       = mark
+    @human_mark = human_mark
+    @board      = board
+  end
+
+  def throw
+    board.print_board
+    puts "Computer turn..."
+    sleep rand(1..2) * 0.5
+    board.position_mark_in_column(choose_location, mark)
+  end
+
+  def choose_location
+    3.times do |i|
+      match_in_rows = check_rows(iteration: i)
+      return match_in_rows if match_in_rows
+
+      match_in_columns = check_columns(iteration: i)
+      return match_in_columns if match_in_columns
+
+      match_in_diagonals = check_diagonals(iteration: i)
+      return match_in_diagonals if match_in_diagonals
+    end
+
+    choose_random_location
+  end
+
+  def check_rows(iteration:)
+    board.grid.each.with_index do |row, index|
+      array = []
+      row.each { |column| array << column }
+
+      computer_marks = array.join.count(mark)
+      human_marks    = array.join.count(human_mark)
+      empty_slots    = array.join.count("-")
+
+      next if empty_slots.zero? && index < 3
+      return false if empty_slots.zero?
+
+      if computer_marks == 3 && human_marks.zero?
+        return array.index("-") + 1
+      end
+
+      if computer_marks == 2 && human_marks.zero? && iteration == 1
+        return array.index("-") + 1
+      end
+
+      if computer_marks == 1 && human_marks.zero? && iteration == 2
+        return array.index("-") + 1
+      end
+
+      next if computer_mark_or_no_human_mark(array) && index < 3
+      return false if computer_mark_or_no_human_mark(array)
+      return false if iteration.zero? && human_marks < 2
+
+      return array.index("-") + 1
+    end
+  end
+
+  def check_columns(iteration:)
+    4.times do |column|
+      array = []
+      board.grid.each.with_index do |_row, index|
+        array << board.grid[index][column]
+      end
+
+      computer_marks = array.join.count(mark)
+      human_marks    = array.join.count(human_mark)
+      empty_slots    = array.join.count("-")
+
+      next if empty_slots.zero?
+
+      if computer_marks == 3 && human_marks.zero?
+        return column + 1
+      end
+
+      if computer_marks == 2 && human_marks.zero? && iteration == 1
+        return column + 1
+      end
+
+      if computer_marks == 1 && human_marks.zero? && iteration == 2
+        return column + 1
+      end
+
+      next if computer_mark_or_no_human_mark(array) && column < 3
+      return false if computer_mark_or_no_human_mark(array)
+      return false if iteration.zero? && human_marks < 2
+
+      return column + 1
+    end
+  end
+
+  def check_diagonals(iteration:)
+    position = diagonal_from_left_bottom(iteration)
+    return position if position
+    position = diagonal_from_right_bottom(iteration)
+    return position if position
+    false
+  end
+
+  def diagonal_from_left_bottom(iteration)
+    array = build_diagonal_left_array
+
+    computer_marks = array.join.count(mark)
+    human_marks    = array.join.count(human_mark)
+    empty_slots    = array.join.count("-")
+    column         = array.index("-")
+
+    return false unless empty_slots || supportive_column_left(column)
+
+    return diagonal_attack(computer_marks, column, iteration) if human_marks.zero?
+    return diagonal_defend(human_marks, column) if computer_marks.zero?
+  end
+
+  def build_diagonal_left_array
+    array  = []
+    column = 0
+    board.grid.reverse.map.with_index do |_row, index|
+      array << board.grid.reverse[index][column]
+      column += 1
+    end
+    array
+  end
+
+  def supportive_column_left(column)
+    array = []
+    board.grid.reverse.each.with_index do |_row, index|
+      array << board.grid.reverse[index][column]
+    end
+    column == array.index("-")
+  end
+
+  def diagonal_from_right_bottom(iteration)
+    array = build_diagonal_right_array
+
+    computer_marks = array.join.count(mark)
+    human_marks    = array.join.count(human_mark)
+    empty_slots    = array.join.count("-")
+    column         = array.count { |v| v == "-" } - 1
+
+    return false unless empty_slots || supportive_column_right(column)
+
+    return diagonal_attack(computer_marks, column, iteration) if human_marks.zero?
+    return diagonal_defend(human_marks, column) if computer_marks.zero?
+  end
+
+  def build_diagonal_right_array
+    array  = []
+    column = 0
+    board.grid.each.with_index do |_row, index|
+      array << board.grid[index][column]
+      column += 1
+    end
+    array
+  end
+
+  def supportive_column_right(column)
+    array = []
+    board.grid.each.with_index do |_row, index|
+      array << board.grid[index][column]
+    end
+
+    column == array.count { |v| v == "-" } - 1
+  end
+
+  def diagonal_attack(computer_marks, column, iteration)
+    case computer_marks
+    when 3 then column + 1
+    when 2 then column + 1 if iteration == 1
+    else false
+    end
+  end
+
+  def diagonal_defend(human_marks, column)
+    case human_marks
+    when 2..3 then column + 1
+    else false
+    end
+  end
+
+  def computer_mark_or_no_human_mark(array)
+    computer_mark = array.any?  { |mark| mark == self.mark }
+    no_human_mark = array.none? { |mark| mark == human_mark }
+    computer_mark || no_human_mark
+  end
+
+  def choose_random_location
+    rows    = [*0..3]
+    columns = [*0..3]
+
+    loop do
+      row      = rows.sample
+      column   = columns.sample
+      location = board.grid[row][column]
+      return column + 1 if location == "-"
+    end
   end
 end
 
@@ -74,7 +270,7 @@ class Game
     @players  = 1
     @human1   = Player.new(name: "Human 1", mark: "X", board: board)
     @human2   = Player.new(name: "Human 2", mark: "O", board: board)
-    @computer = Computer.new(mark: "O", board: board)
+    @computer = Computer.new(mark: "O", human_mark: human1.mark, board: board)
   end
 
   def setup
@@ -100,7 +296,7 @@ class Game
   def second_turn
     case players
     when 1
-      computer.throw(introduce_position(computer))
+      computer.throw
       check_for_winner(computer)
     when 2
       human2.throw(introduce_position(human2))
