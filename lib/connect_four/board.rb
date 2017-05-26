1dca08fbb760449c9a633fd6f98c6d7fc4857d51
lujanfernaud <lujanfernaud@gmail.com>
2017-05-26 19:21:22 +0100

    Increase board size

--- lib/connect_four/board.rb
+++ lib/connect_four/board.rb
@@ -8,7 +8,7 @@ class Board
   end
 
   def create_grid
-    @grid = Array.new(4) { Array.new(4) { "-" } }
+    @grid = Array.new(6) { Array.new(7) { "-" } }
   end
 
   def reset
@@ -18,21 +18,25 @@ class Board
   def print_board
     system "clear" or system "cls"
     puts "\n"
-    puts "   #################"
-    puts "   #               #"
-    puts "   #   CONNECT 4   #"
-    puts "   #               #"
-    puts "   #################"
+    puts "   #############################"
+    puts "   #                           #"
+    puts "   #         CONNECT 4         #"
+    puts "   #                           #"
+    puts "   #############################"
     puts "\n"
-    puts "     1 | 2 | 3 | 4 "
-    puts "   -----------------"
-    puts "   | #{grid[0][0]}   #{grid[0][1]}   #{grid[0][2]}   #{grid[0][3]} |"
-    puts "   ----------------"
-    puts "   | #{grid[1][0]}   #{grid[1][1]}   #{grid[1][2]}   #{grid[1][3]} |"
-    puts "   ----------------"
-    puts "   | #{grid[2][0]}   #{grid[2][1]}   #{grid[2][2]}   #{grid[2][3]} |"
-    puts "   ----------------"
-    puts "   | #{grid[3][0]}   #{grid[3][1]}   #{grid[3][2]}   #{grid[3][3]} |"
+    puts "     1 | 2 | 3 | 4 | 5 | 6 | 7"
+    puts "   -----------------------------"
+    puts "   | #{grid[0][0]}   #{grid[0][1]}   #{grid[0][2]}   #{grid[0][3]}   #{grid[0][4]}   #{grid[0][5]}   #{grid[0][6]} |"
+    puts "   -----------------------------"
+    puts "   | #{grid[1][0]}   #{grid[1][1]}   #{grid[1][2]}   #{grid[1][3]}   #{grid[1][4]}   #{grid[1][5]}   #{grid[1][6]} |"
+    puts "   -----------------------------"
+    puts "   | #{grid[2][0]}   #{grid[2][1]}   #{grid[2][2]}   #{grid[2][3]}   #{grid[2][4]}   #{grid[2][5]}   #{grid[2][6]} |"
+    puts "   -----------------------------"
+    puts "   | #{grid[3][0]}   #{grid[3][1]}   #{grid[3][2]}   #{grid[3][3]}   #{grid[3][4]}   #{grid[3][5]}   #{grid[3][6]} |"
+    puts "   -----------------------------"
+    puts "   | #{grid[4][0]}   #{grid[4][1]}   #{grid[4][2]}   #{grid[4][3]}   #{grid[4][4]}   #{grid[4][5]}   #{grid[4][6]} |"
+    puts "   -----------------------------"
+    puts "   | #{grid[5][0]}   #{grid[5][1]}   #{grid[5][2]}   #{grid[5][3]}   #{grid[5][4]}   #{grid[5][5]}   #{grid[5][6]} |"
     puts "\n"
   end
 
