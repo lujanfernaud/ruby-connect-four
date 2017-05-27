b6d54532b2c48ad969757b71d84a8f6fe79c3d15
lujanfernaud <lujanfernaud@gmail.com>
2017-05-27 14:19:40 +0100

    Refactor #throw and delete specs for private methods

--- spec/computer_spec.rb
+++ spec/computer_spec.rb
@@ -26,355 +26,105 @@ describe Computer do
   end
 
   describe "#throw" do
-    it "prints 'Computer turn...' and places mark on board" do
+    before do
       expect(@board).to receive(:print_board)
       expect(STDOUT).to receive(:puts).with("Computer turn...")
       allow(@computer).to receive(:sleep)
-      expect(@board).to receive(:position_mark_in_column)
-      @computer.throw
     end
-  end
 
-  describe "#choose_location" do
-  end
-
-  describe "#check_rows" do
-    context "on first iteration" do
-      context "with 1 human mark" do
-        it "returns 'false'" do
-          @board.grid = [["-", "-", "-", "-"],
-                         ["-", "-", "-", "-"],
-                         ["-", "-", "-", "-"],
-                         ["-", "X", "-", "-"]]
-          expect(@computer.check_rows(iteration: 0)).to eql(false)
-          @computer.check_rows(iteration: 0)
-        end
+    context "with 2 consecutive human marks in a row" do
+      it "places mark next to human mark" do
+        @board.grid = [["-", "-", "-", "-", "-", "-", "-"],
+                       ["-", "-", "-", "-", "-", "-", "-"],
+                       ["-", "-", "-", "-", "-", "-", "-"],
+                       ["-", "-", "-", "-", "-", "-", "-"],
+                       ["-", "-", "-", "-", "-", "O", "O"],
+                       ["-", "-", "-", "-", "-", "X", "X"]]
+        @computer.throw
+        expect(@board.grid[5][4]).to eql("O")
       end
 
-      context "with 2 human marks" do
-        it "returns index of first free position next to human" do
-          @board.grid = [["-", "-", "-", "-"],
-                         ["-", "-", "-", "-"],
-                         ["O", "-", "-", "-"],
-                         ["X", "X", "-", "-"]]
-          expect(@computer.check_rows(iteration: 0)).to eql(3)
-          @computer.check_rows(iteration: 0)
-        end
-      end
-
-      context "with 3 human marks" do
-        it "returns index + 1 of first free position next to human" do
-          @board.grid = [["-", "-", "-", "-"],
-                         ["-", "-", "-", "-"],
-                         ["O", "O", "-", "-"],
-                         ["X", "X", "X", "-"]]
-          expect(@computer.check_rows(iteration: 0)).to eql(4)
-          @computer.check_rows(iteration: 0)
-        end
-      end
-
-      context "with 3 computer marks and 0 human marks" do
-        it "returns index + 1 of first free position next to computer" do
-          @board.grid = [["O", "O", "O", "-"],
-                         ["X", "O", "X", "X"],
-                         ["O", "O", "X", "X"],
-                         ["X", "X", "O", "O"]]
-          expect(@computer.check_rows(iteration: 0)).to eql(4)
-          @computer.check_rows(iteration: 0)
-        end
+      it "places mark next to human mark" do
+        @board.grid = [["-", "-", "-", "-", "-", "-", "-"],
+                       ["-", "-", "-", "-", "-", "-", "-"],
+                       ["-", "-", "-", "-", "-", "-", "-"],
+                       ["-", "-", "-", "-", "-", "-", "-"],
+                       ["-", "-", "-", "O", "-", "-", "-"],
+                       ["-", "-", "X", "X", "-", "-", "-"]]
+        @computer.throw
+        expect([@board.grid[5][1], @board.grid[5][4]]).to include("O")
       end
     end
 
-    context "on second iteration" do
-      context "with 1 human mark" do
-        it "returns index + 1 of first free position next to human" do
-          @board.grid = [["-", "-", "-", "-"],
-                         ["-", "-", "-", "-"],
-                         ["-", "-", "-", "-"],
-                         ["-", "X", "-", "-"]]
-          expect(@computer.check_rows(iteration: 1)).to eql(1)
-          @computer.check_rows(iteration: 1)
-        end
+    context "with 3 consecutive human marks in a row" do
+      it "places mark next to human mark" do
+        @board.grid = [["-", "-", "-", "-", "-", "-", "-"],
+                       ["-", "-", "-", "-", "-", "-", "-"],
+                       ["-", "-", "-", "-", "-", "-", "-"],
+                       ["-", "-", "-", "-", "-", "-", "-"],
+                       ["-", "-", "-", "-", "-", "O", "O"],
+                       ["-", "-", "-", "-", "X", "X", "X"]]
+        @computer.throw
+        expect(@board.grid[5][3]).to eql("O")
       end
 
-      context "with 2 human marks" do
-        it "returns index + 1 of first free position next to human" do
-          @board.grid = [["-", "-", "-", "-"],
-                         ["-", "-", "-", "-"],
-                         ["-", "O", "-", "-"],
-                         ["X", "X", "-", "-"]]
-          expect(@computer.check_rows(iteration: 1)).to eql(3)
-          @computer.check_rows(iteration: 1)
-        end
-      end
-
-      context "with 2 computer marks and 0 human marks" do
-        it "returns index + 1 of first free position next to computer" do
-          @board.grid = [["-", "-", "-", "-"],
-                         ["-", "-", "-", "-"],
-                         ["O", "O", "-", "-"],
-                         ["X", "X", "O", "X"]]
-          expect(@computer.check_rows(iteration: 1)).to eql(3)
-          @computer.check_rows(iteration: 1)
-        end
-      end
-
-      context "with 2 computer marks and 1 human mark" do
-        it "returns 'false'" do
-          @board.grid = [["-", "-", "-", "-"],
-                         ["-", "-", "-", "-"],
-                         ["O", "O", "-", "X"],
-                         ["X", "X", "O", "X"]]
-          expect(@computer.check_rows(iteration: 1)).to eql(false)
-          @computer.check_rows(iteration: 1)
-        end
+      it "places mark next to human mark" do
+        @board.grid = [["-", "-", "-", "-", "-", "-", "-"],
+                       ["-", "-", "-", "-", "-", "-", "-"],
+                       ["-", "-", "-", "-", "-", "-", "-"],
+                       ["-", "-", "-", "-", "-", "-", "-"],
+                       ["-", "-", "O", "O", "-", "-", "-"],
+                       ["-", "-", "X", "X", "X", "-", "-"]]
+        @computer.throw
+        expect([@board.grid[5][1], @board.grid[5][5]]).to include("O")
       end
     end
 
-    context "on third iteration" do
-      context "with 1 computer mark and 0 human marks" do
-        it "returns index + 1 of first free position next to computer" do
-          @board.grid = [["-", "-", "-", "-"],
-                         ["O", "-", "-", "-"],
-                         ["O", "O", "X", "X"],
-                         ["X", "X", "O", "X"]]
-          expect(@computer.check_rows(iteration: 2)).to eql(2)
-          @computer.check_rows(iteration: 2)
-        end
-      end
-    end
-  end
-
-  describe "#check_columns" do
-    context "on first iteration" do
-      context "with no empty slots" do
-        it "returns 'false'" do
-          @board.grid = [["X", "O", "X", "-"],
-                         ["O", "O", "X", "O"],
-                         ["X", "X", "O", "O"],
-                         ["O", "X", "X", "X"]]
-          expect(@computer.check_columns(iteration: 0)).to eql(false)
-          @computer.check_columns(iteration: 0)
-        end
+    context "with 2 consecutive computer marks in a row" do
+      it "places mark next to computer mark" do
+        @board.grid = [["-", "-", "-", "-", "-", "-", "-"],
+                       ["-", "-", "-", "-", "-", "-", "-"],
+                       ["-", "-", "-", "-", "-", "-", "-"],
+                       ["-", "-", "-", "-", "-", "-", "-"],
+                       ["-", "-", "-", "-", "-", "O", "O"],
+                       ["X", "-", "-", "-", "O", "X", "X"]]
+        @computer.throw
+        expect(@board.grid[4][4]).to eql("O")
       end
 
-      context "with 1 human mark" do
-        it "returns 'false'" do
-          @board.grid = [["-", "-", "-", "-"],
-                         ["-", "-", "-", "-"],
-                         ["-", "-", "-", "-"],
-                         ["-", "X", "-", "-"]]
-          expect(@computer.check_columns(iteration: 0)).to eql(false)
-          @computer.check_columns(iteration: 0)
-        end
-      end
-
-      context "with 2 human marks" do
-        it "returns column + 1" do
-          @board.grid = [["-", "-", "-", "-"],
-                         ["-", "-", "-", "-"],
-                         ["-", "-", "O", "X"],
-                         ["-", "O", "X", "X"]]
-          expect(@computer.check_columns(iteration: 0)).to eql(4)
-          @computer.check_columns(iteration: 0)
-        end
-      end
-
-      context "with 3 human marks" do
-        it "returns column + 1" do
-          @board.grid = [["-", "-", "-", "-"],
-                         ["X", "O", "-", "O"],
-                         ["X", "O", "O", "X"],
-                         ["X", "O", "X", "X"]]
-          expect(@computer.check_columns(iteration: 0)).to eql(1)
-          @computer.check_columns(iteration: 0)
-        end
-      end
-
-      context "with 3 computer marks and 0 human marks" do
-        it "returns column + 1" do
-          @board.grid = [["O", "-", "-", "-"],
-                         ["X", "O", "X", "O"],
-                         ["X", "O", "O", "X"],
-                         ["X", "O", "X", "X"]]
-          expect(@computer.check_columns(iteration: 0)).to eql(2)
-          @computer.check_columns(iteration: 0)
-        end
+      it "places mark next to computer mark" do
+        @board.grid = [["-", "-", "-", "-", "-", "-", "-"],
+                       ["-", "-", "-", "-", "-", "-", "-"],
+                       ["-", "-", "-", "-", "-", "-", "-"],
+                       ["-", "-", "-", "-", "-", "-", "-"],
+                       ["-", "-", "-", "-", "-", "-", "-"],
+                       ["-", "-", "O", "O", "-", "X", "X"]]
+        @computer.throw
+        expect([@board.grid[5][1], @board.grid[5][4]]).to include("O")
       end
     end
 
-    context "on second iteration" do
-      context "with 1 human mark" do
-        it "returns column + 1" do
-          @board.grid = [["-", "-", "-", "-"],
-                         ["-", "-", "-", "-"],
-                         ["-", "-", "-", "-"],
-                         ["-", "X", "-", "-"]]
-          expect(@computer.check_columns(iteration: 1)).to eql(2)
-          @computer.check_columns(iteration: 1)
-        end
+    context "with 3 consecutive computer marks in a row" do
+      it "places mark next to computer mark" do
+        @board.grid = [["-", "-", "-", "-", "-", "-", "-"],
+                       ["-", "-", "-", "-", "-", "-", "-"],
+                       ["-", "-", "-", "-", "-", "-", "-"],
+                       ["-", "-", "-", "-", "-", "-", "-"],
+                       ["-", "-", "-", "-", "O", "O", "O"],
+                       ["X", "-", "-", "X", "O", "X", "X"]]
+        @computer.throw
+        expect(@board.grid[4][3]).to eql("O")
       end
 
-      context "with 2 human marks" do
-        it "returns column + 1" do
-          @board.grid = [["-", "-", "-", "-"],
-                         ["-", "-", "-", "-"],
-                         ["X", "O", "-", "-"],
-                         ["X", "X", "O", "-"]]
-          expect(@computer.check_columns(iteration: 1)).to eql(1)
-          @computer.check_columns(iteration: 1)
-        end
-      end
-    end
-
-    context "on third iteration" do
-      context "with 1 computer mark and 0 human marks" do
-        it "returns column + 1" do
-          @board.grid = [["-", "-", "-", "-"],
-                         ["O", "-", "-", "-"],
-                         ["X", "O", "-", "-"],
-                         ["X", "X", "O", "X"]]
-          expect(@computer.check_columns(iteration: 2)).to eql(3)
-          @computer.check_columns(iteration: 2)
-        end
-      end
-    end
-  end
-
-  describe "#check_diagonals" do
-    context "on first iteration" do
-      context "with no empty slots (from left bottom)" do
-        it "returns 'false'" do
-          @board.grid = [["-", "-", "-", "X"],
-                         ["O", "O", "O", "X"],
-                         ["X", "O", "X", "O"],
-                         ["O", "X", "X", "X"]]
-          expect(@computer.check_diagonals).to eql(false)
-          @computer.check_diagonals
-        end
-      end
-
-      context "with no empty slots (from right bottom)" do
-        it "returns 'false'" do
-          @board.grid = [["X", "O", "X", "-"],
-                         ["O", "O", "X", "O"],
-                         ["X", "X", "O", "O"],
-                         ["O", "X", "X", "X"]]
-          expect(@computer.check_diagonals).to eql(false)
-          @computer.check_diagonals
-        end
-      end
-
-      context "with 2 human marks (from left bottom)" do
-        it "defends" do
-          @board.grid = [["-", "-", "-", "-"],
-                         ["O", "-", "-", "-"],
-                         ["X", "X", "O", "O"],
-                         ["X", "X", "O", "X"]]
-          expect(@computer.check_diagonals).to eql(3)
-          @computer.check_diagonals
-        end
-      end
-
-      context "with 2 human marks (from right bottom)" do
-        it "defends" do
-          @board.grid = [["-", "-", "-", "-"],
-                         ["O", "-", "-", "-"],
-                         ["X", "O", "X", "O"],
-                         ["X", "X", "O", "X"]]
-          expect(@computer.check_diagonals).to eql(2)
-          @computer.check_diagonals
-        end
-      end
-
-      context "with 3 human marks (from left bottom)" do
-        it "defends" do
-          @board.grid = [["-", "-", "-", "-"],
-                         ["O", "O", "X", "O"],
-                         ["X", "X", "O", "O"],
-                         ["X", "X", "O", "X"]]
-          expect(@computer.check_diagonals).to eql(4)
-          @computer.check_diagonals
-        end
-      end
-
-      context "with 3 human marks (from right bottom)" do
-        it "defends" do
-          @board.grid = [["-", "-", "-", "-"],
-                         ["O", "X", "O", "-"],
-                         ["X", "O", "X", "O"],
-                         ["X", "X", "O", "X"]]
-          expect(@computer.check_diagonals).to eql(1)
-          @computer.check_diagonals
-        end
-      end
-
-      context "with 3 computer marks (from left bottom)" do
-        it "attacks and wins" do
-          @board.grid = [["-", "-", "-", "-"],
-                         ["O", "X", "O", "X"],
-                         ["X", "O", "X", "O"],
-                         ["O", "X", "X", "X"]]
-          expect(@computer.check_diagonals).to eql(4)
-          @computer.check_diagonals
-        end
-      end
-
-      context "with 3 computer marks (from right bottom)" do
-        it "attacks and wins" do
-          @board.grid = [["-", "-", "-", "-"],
-                         ["O", "O", "X", "X"],
-                         ["X", "X", "O", "O"],
-                         ["X", "X", "X", "O"]]
-          expect(@computer.diagonal_from_right_bottom(0)).to eql(1)
-          @computer.diagonal_from_right_bottom(0)
-        end
-      end
-    end
-
-    context "on second iteration" do
-      context "with 2 human marks (from left bottom)" do
-        it "defends" do
-          @board.grid = [["-", "-", "-", "-"],
-                         ["O", "-", "-", "-"],
-                         ["X", "X", "O", "O"],
-                         ["X", "X", "O", "X"]]
-          expect(@computer.check_diagonals).to eql(3)
-          @computer.check_diagonals
-        end
-      end
-
-      context "with 2 human marks (from right bottom)" do
-        it "defends" do
-          @board.grid = [["-", "-", "-", "-"],
-                         ["O", "-", "-", "-"],
-                         ["X", "O", "X", "O"],
-                         ["O", "X", "O", "X"]]
-          expect(@computer.check_diagonals).to eql(2)
-          @computer.check_diagonals
-        end
-      end
-
-      context "with 2 computer marks (from left bottom)" do
-        it "defends" do
-          @board.grid = [["-", "-", "-", "-"],
-                         ["O", "-", "-", "-"],
-                         ["X", "O", "X", "O"],
-                         ["O", "X", "X", "X"]]
-          expect(@computer.check_diagonals).to eql(2)
-          @computer.check_diagonals
-        end
-      end
-
-      context "with 2 computer marks (from right bottom)" do
-        it "defends" do
-          @board.grid = [["-", "-", "-", "-"],
-                         ["O", "-", "-", "-"],
-                         ["X", "X", "O", "O"],
-                         ["X", "X", "X", "O"]]
-          expect(@computer.check_diagonals).to eql(3)
-          @computer.check_diagonals
-        end
+      it "places mark next to computer mark" do
+        @board.grid = [["-", "-", "-", "-", "-", "-", "-"],
+                       ["-", "-", "-", "-", "-", "-", "-"],
+                       ["-", "-", "-", "-", "-", "-", "-"],
+                       ["-", "-", "-", "-", "-", "-", "-"],
+                       ["X", "-", "-", "-", "-", "-", "-"],
+                       ["X", "-", "O", "O", "O", "-", "X"]]
+        @computer.throw
+        expect([@board.grid[5][1], @board.grid[5][5]]).to include("O")
       end
     end
   end
