7956f511ecdf3bfa13bd94996f0e2f269dd12521
lujanfernaud <lujanfernaud@gmail.com>
2017-05-28 22:35:15 +0100

    Check if column is available before throwing

--- lib/connect_four/player.rb
+++ lib/connect_four/player.rb
@@ -1,3 +1,5 @@
+require "pry"
+
 class Player
   attr_accessor :name
   attr_reader   :board, :mark
@@ -9,6 +11,9 @@ class Player
   end
 
   def throw(column)
+    return board.the_column_is_full(column, self) unless
+      board.column_available?(column.to_i)
+
     board.position_mark_in_column(column.to_i, self)
   end
 end
