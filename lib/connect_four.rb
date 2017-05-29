668ab76eb51889cc1c6c2894dd8f3443ac975ea6
lujanfernaud <lujanfernaud@gmail.com>
2017-05-29 13:38:55 +0100

    Require 'judge'

--- lib/connect_four.rb
+++ lib/connect_four.rb
@@ -1,4 +1,5 @@
 require_relative "connect_four/player"
 require_relative "connect_four/computer"
 require_relative "connect_four/board"
+require_relative "connect_four/judge"
 require_relative "connect_four/game"
