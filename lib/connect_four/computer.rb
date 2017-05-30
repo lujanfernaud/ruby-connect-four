6023cfc7cce6c995524f92f0bdb2f8a30ada481c
lujanfernaud <lujanfernaud@gmail.com>
2017-05-30 21:35:09 +0100

    Complete refactor

--- lib/connect_four/computer.rb
+++ lib/connect_four/computer.rb
@@ -1,12 +1,12 @@
 class Computer
   attr_accessor :name
-  attr_reader   :mark, :board, :human_mark
+  attr_reader   :mark, :board, :human
 
-  def initialize(mark:, human_mark:, board: [])
-    @name       = "Computer"
-    @mark       = mark
-    @human_mark = human_mark
-    @board      = board
+  def initialize(mark:, human:, board: [])
+    @name  = "Computer"
+    @mark  = mark
+    @human = human
+    @board = board
   end
 
   def throw
@@ -18,195 +18,233 @@ class Computer
 
   private
 
-  # TO-DO: Check if the position below the one selected is empty.
   def choose_column
-    3.times do |i|
-      matches = []
-      matches << check_rows(iteration: i)
-      matches << check_columns(iteration: i)
-      matches << check_diagonals(iteration: i)
+    matches = []
+    matches << check_rows
+    matches << check_columns
+    matches << check_diagonals
 
-      good_match = proc { |match| match && board.column_available?(match) }
-      column = matches.select(&good_match).first
-      return column if column
-    end
+    good_match = proc { |match| match && board.column_available?(match) }
+    column     = matches.select(&good_match).first
 
+    return column + 1 if column
     choose_random_column
   end
 
-  def check_rows(iteration:)
+  def check_rows
     matches = { human: {}, computer: {} }
-    empty   = proc { |slot| slot == "-" }
 
-    board.grid.each.with_index do |row, index|
-      empty_slots = row.join.count("-")
+    board.grid.each.with_index do |row, row_index|
+      get_row_matches(row, row_index, matches)
 
-      if empty_slots == 7
-        case index
-        when 0..4 then next
-        when 5    then return false
-        end
-      end
+      next if row_index < 5
 
-      1.upto(3) do |count|
-        human    = Array.new(count) { "X" }
-        computer = Array.new(count) { "O" }
-        players  = { human: human, computer: computer }
-
-        players.each do |player_name, player_marks|
-          idx = row.each_cons(count).map { |m| m == player_marks }.index(true)
-          next if idx.nil?
-
-          case count
-          when 1
-            next if iteration < 2
-
-            if idx > 0 && [row[idx - 1], row[idx + 1]].all?(&empty)
-              matches[player_name][count] = [idx - 1, idx + 1].sample
-
-            elsif idx > 1 && row[(idx - 2)..(idx - 1)].all?(&empty)
-
-              matches[player_name][count] = idx - count
-
-            elsif row[(idx + count)..(idx + (count + 1))].all?(&empty)
-
-              matches[player_name][count] = idx + count
-            end
-
-          when 2
-            next if iteration.zero?
-
-            if idx > 0 && [row[idx - 1], row[idx + 2]].all?(&empty)
-              matches[player_name][count] = [idx - 1, idx + 2].sample
-
-            elsif idx > 1 && row[(idx - 2)..(idx - 1)].all?(&empty) ||
-                  row[idx - 1] == "-" && row[idx - 2] == "X"
-
-              matches[player_name][count] = idx - (count - 1)
-
-            elsif row[(idx + count)..(idx + (count + 1))].all?(&empty) ||
-                  row[idx + count] == "-" && row[idx + count + 1] == "X"
-
-              matches[player_name][count] = idx + count
-
-            end
-
-          when 3
-
-            if idx > 0 && [row[idx - 1], row[idx + 3]].all?(&empty)
-              matches[player_name][count] = [idx - 1, idx + 3].sample
-            elsif idx > 1 && row[idx - 1] == "-"
-              matches[player_name][count] = idx - 1
-            elsif row[idx + 3] == "-"
-              matches[player_name][count] = idx + 3
-            end
-          end
-        end
-      end
-
-      next if index < 5
-
-      columns = [matches[:computer][3], matches[:human][3],
-                 matches[:computer][2], matches[:human][2], matches[:human][1]]
+      columns = parse_matches(matches)
       column  = columns.compact.first
 
-      return column + 1 if column
+      return column if column
       return false
     end
   end
 
-  def check_columns(iteration:)
+  def get_row_matches(row, row_index, matches)
+    get_row_human_match(row, row_index, matches)
+    get_row_computer_match(row, row_index, matches)
+  end
+
+  def get_row_human_match(row, row_index, matches)
+    human_data = parse_row_marks(human, row)
+    return false unless human_data
+
+    count        = human_data[0]
+    regexp       = human_data[1]
+    empty_index  = human_data[2]
+    human_index  = get_index(row, regexp)
+    column       = human_index + empty_index
+
+    return false if position_below_empty?(row_index, column)
+    matches[:human][count] = column
+  end
+
+  def get_row_computer_match(row, row_index, matches)
+    computer_data = parse_row_marks(self, row)
+    return false unless computer_data
+
+    count          = computer_data[0]
+    regexp         = computer_data[1]
+    empty_index    = computer_data[2]
+    computer_index = get_index(row, regexp)
+    column         = computer_index + empty_index
+
+    return false if position_below_empty?(row_index, column)
+    matches[:computer][count] = column
+  end
+
+  def parse_row_marks(player, line)
+    regexps_for_row(player).each do |regexp|
+      match = line.join.match(regexp)
+      next unless match
+      marks_count = match[0].count(player.mark)
+      empty_index = match[0] =~ /\-/
+      return [marks_count, regexp, empty_index] if marks_count
+    end
+    false
+  end
+
+  def regexps(player)
+    [/#{player.mark}#{player.mark}#{player.mark}\-/,  # "XXX-"
+     /#{player.mark}#{player.mark}\-\-/]              # "XX--"
+  end
+
+  def regexps_for_row(player)
+    [regexps_three_marks_for_row(player),
+     regexps_two_marks_for_row(player)].flatten
+  end
+
+  def regexps_three_marks_for_row(player)
+    [/#{player.mark}#{player.mark}\-#{player.mark}/,  # "XX-X"
+     /#{player.mark}\-#{player.mark}#{player.mark}/,  # "X-XX"
+     /#{player.mark}#{player.mark}#{player.mark}\-/,  # "XXX-"
+     /\-#{player.mark}#{player.mark}#{player.mark}/]  # "-XXX"
+  end
+
+  def regexps_two_marks_for_row(player)
+    [/#{player.mark}\-\-#{player.mark}/,              # "X--X"
+     /\-#{player.mark}\-#{player.mark}/,              # "-X-X"
+     /#{player.mark}\-#{player.mark}\-/,              # "X-X-"
+     /#{player.mark}#{player.mark}\-\-/,              # "XX--"
+     /\-#{player.mark}#{player.mark}\-/,              # "-XX-"
+     /\-\-#{player.mark}#{player.mark}/]              # "--XX"
+  end
+
+  def check_columns
     matches = { human: {}, computer: {} }
 
     7.times do |col|
-      array = board.grid.map.with_index { |_row, index| board.grid[index][col] }
-      array.reverse!
+      array = build_column_array(col)
 
-      empty_slots = array.join.count("-")
-      next if empty_slots.zero? || empty_slots == 7
-
-      2.upto(3) do |count|
-        human    = Array.new(count) { "X" }
-        computer = Array.new(count) { "O" }
-        players  = { human: human, computer: computer }
-
-        players.each do |player_name, player_marks|
-          idx = array.each_cons(count).map { |m| m == player_marks }.index(true)
-          next if idx.nil?
-          case count
-          when 2
-            next if iteration.zero?
-            matches[player_name][count] = col if array[idx + count] == "-"
-          when 3
-            matches[player_name][count] = col if array[idx + count] == "-"
-          end
-        end
-      end
+      get_matches(array, col, matches)
 
       next if col < 6
 
-      columns = [matches[:computer][3], matches[:human][3],
-                 matches[:human][2], matches[:computer][2]]
+      columns = parse_matches(matches)
       column  = columns.compact.first
 
-      return column + 1 if column
+      return column if column
       return false
     end
   end
 
-  def check_diagonals(iteration:)
+  def build_column_array(col)
+    board.grid.map.with_index { |_row, index| board.grid[index][col] }.reverse
+  end
+
+  def get_matches(line, column, matches)
+    get_human_match(line, column, matches)
+    get_computer_match(line, column, matches)
+  end
+
+  def get_human_match(line, column, matches)
+    count = count_marks(human, line)
+    return false unless count
+    matches[:human][count] = column
+  end
+
+  def get_computer_match(line, column, matches)
+    count = count_marks(self, line)
+    return false unless count
+    matches[:computer][count] = column
+  end
+
+  def count_marks(player, line)
+    regexps(player).each do |regexp|
+      match = line.join.match(regexp)
+      next unless match
+      marks_count = match[0].count(player.mark)
+      return marks_count if marks_count
+    end
+    false
+  end
+
+  def check_diagonals
     matches = { human: {}, computer: {} }
 
     board.diagonals.each do |side|
       diagonals = build_diagonals_from(side)
 
-      diagonals.each.with_index do |diagonal, index|
+      diagonals.each do |diagonal|
         col   = diagonal[0]
-        marks = diagonal[1..-1]
+        row   = diagonal[1]
+        marks = diagonal[2..-1]
         marks.reverse! if side[:side] == :right
 
-        empty_slots = marks.join.count("-")
-
-        if empty_slots.zero?
-          case index
-          when 0..4 then next
-          when 5    then return false
-          end
-        end
-
-        2.upto(3) do |count|
-          human    = Array.new(count) { "X" }
-          computer = Array.new(count) { "O" }
-          players  = { human: human, computer: computer }
-
-          players.each do |player_name, player_marks|
-            idx = marks.each_cons(count).map { |m| m == player_marks }.index(true)
-            next if idx.nil?
-            case count
-            when 2
-              next if iteration.zero?
-              matches[player_name][count] = col + (idx + count) if marks[idx + count] == "-"
-            when 3
-              matches[player_name][count] = col + (idx + count) if marks[idx + count] == "-"
-            end
-          end
-        end
+        get_diagonal_matches(marks, col, row, matches, side[:side])
       end
-
       next if side[:side] == :left
 
-      return false if matches[:human].empty? && matches[:computer].empty?
-
-      columns = [matches[:computer][3], matches[:human][3],
-                 matches[:human][2], matches[:computer][2]]
+      columns = parse_matches(matches)
       column  = columns.compact.first
 
-      return column + 1 if column
+      return column if column
       return false
     end
   end
 
+  def get_diagonal_matches(line, column, row, matches, side)
+    get_diagonal_human_match(line, column, row, matches, side)
+    get_diagonal_computer_match(line, column, row, matches, side)
+  end
+
+  def get_diagonal_human_match(line, col, row, matches, side)
+    human_data = parse_diagonal_marks(human, line)
+    return false unless human_data
+
+    count       = human_data[0]
+    regexp      = human_data[1]
+    human_index = get_index(line, regexp)
+    row_index   = row - count if side == :left
+    row_index   = row - count - human_index if side == :right
+    column      = col + count + human_index if side == :left
+    column      = col - count - human_index if side == :right
+
+    return false if position_below_empty?(row_index, column)
+    matches[:human][count] = column unless matches[:human][count]
+  end
+
+  def get_diagonal_computer_match(line, col, row, matches, side)
+    computer_data = parse_diagonal_marks(self, line)
+    return false unless computer_data
+
+    count          = computer_data[0]
+    regexp         = computer_data[1]
+    computer_index = get_index(line, regexp)
+    row_index      = row - count if side == :left
+    row_index      = row - count - computer_index if side == :right
+    column         = col + count + computer_index if side == :left
+    column         = col - count - computer_index if side == :right
+
+    return false if position_below_empty?(row_index, column)
+    matches[:computer][count] = column unless matches[:computer][count]
+  end
+
+  def parse_diagonal_marks(player, line)
+    regexps_for_row(player).each do |regexp|
+      match = line.join.match(regexp)
+      next unless match
+      marks_count = match[0].count(player.mark)
+      return [marks_count, regexp] if marks_count
+    end
+    false
+  end
+
   def build_diagonals_from(side)
+    case side[:side]
+    when :left  then build_diagonals_from_left(side)
+    when :right then build_diagonals_from_right(side)
+    end
+  end
+
+  def build_diagonals_from_left(side)
     diagonals = []
     side.each_value do |pair|
       next if pair == :left || pair == :right
@@ -214,25 +252,64 @@ class Computer
       y = pair[:start][:y]
       x = pair[:start][:x]
 
-      add_diagonal(pair, side[:side], y, x, diagonals)
+      add_diagonal_from_left(pair, y, x, diagonals)
     end
     diagonals
   end
 
-  def add_diagonal(diagonal, side, y, x, diagonals)
+  def build_diagonals_from_right(side)
+    diagonals = []
+    side.each_value do |pair|
+      next if pair == :left || pair == :right
+
+      y = pair[:finish][:y]
+      x = pair[:finish][:x]
+
+      add_diagonal_from_right(pair, y, x, diagonals)
+    end
+    diagonals
+  end
+
+  def add_diagonal_from_left(diagonal, y, x, diagonals)
     start    = diagonal[:start][:x]
     finish   = diagonal[:finish][:x]
-    diagonal = [start]
+    row      = diagonal[:start][:y]
+    diagonal = [start, row]
 
     (start..finish).each do
       diagonal << board.grid[y][x]
-      y  = case side
-           when :left  then y - 1
-           when :right then y + 1
-           end
+      y -= 1
       x += 1
     end
     diagonals << diagonal
+  end
+
+  def add_diagonal_from_right(diagonal, y, x, diagonals)
+    start    = diagonal[:finish][:x]
+    finish   = diagonal[:start][:x]
+    row      = diagonal[:start][:y]
+    diagonal = [finish, row]
+
+    (start..finish).each do
+      diagonal << board.grid[y][x]
+      y += 1
+      x += 1
+    end
+    diagonals << diagonal
+  end
+
+  def get_index(line, regexp)
+    line.join =~ regexp
+  end
+
+  def position_below_empty?(row, column)
+    return false if row == 5
+    board.grid[row + 1][column] == "-"
+  end
+
+  def parse_matches(matches)
+    [matches[:computer][3], matches[:human][3],
+     matches[:computer][2], matches[:human][2]]
   end
 
   def choose_random_column
