f7b0b44c3050b9882de2ad169311d85f04942e22
lujanfernaud <lujanfernaud@gmail.com>
2017-05-31 15:19:17 +0100

    Reorder objects

--- lib/connect_four.rb
+++ lib/connect_four.rb
@@ -1,5 +1,5 @@
-require_relative "connect_four/player"
+require_relative "connect_four/gamesetup"
+require_relative "connect_four/game"
 require_relative "connect_four/board"
 require_relative "connect_four/judge"
-require_relative "connect_four/game"
-require_relative "connect_four/gamesetup"
+require_relative "connect_four/player"
