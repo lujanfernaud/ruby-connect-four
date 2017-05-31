10ba8687d4a4f7079297ea79a828edd9cf40e602
lujanfernaud <lujanfernaud@gmail.com>
2017-05-31 18:09:19 +0100

    Do not check for column availability in #throw

--- lib/connect_four/player.rb
+++ lib/connect_four/player.rb
@@ -8,9 +8,6 @@ class Player
   end
 
   def throw(column, board)
-    return board.the_column_is_full(column, self) unless
-      board.column_available?(column.to_i)
-
     board.position_mark_in_column(column.to_i, self)
   end
 end
