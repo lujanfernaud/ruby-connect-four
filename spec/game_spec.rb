2a5f2307b2e2f56ffebf617b139a26f616bc2027
lujanfernaud <lujanfernaud@gmail.com>
2017-06-07 18:19:45 +0100

    Refactor
    Remove require for 'spec_helper'
    Use 'described_class' instead of class name
    Use #all? instead of #each
    Use spies

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -1,10 +1,8 @@
-require "spec_helper"
-
 describe Game do
   let(:player1) { Player.new(name: "Sandi", mark: "X") }
   let(:player2) { Player.new(name: "Matz", mark: "O") }
   let(:players) { [player1, player2] }
-  let(:game)    { Game.new(players) }
+  let(:game)    { described_class.new(players) }
   let(:printer) { game.printer }
 
   describe "attributes" do
@@ -17,25 +15,31 @@ describe Game do
     end
 
     it "each player is a Player object" do
-      players.each { |player| expect(player).to be_a(Player) }
+      players.all? { |player| expect(player).to be_a(Player) }
     end
   end
 
   describe "#start_game" do
     it "gives turns to players" do
-      expect(game).to receive(:players_turns)
+      allow(game).to receive(:players_turns)
       game.start_game
+      expect(game).to have_received(:players_turns)
     end
   end
 
   describe "#retry_turn" do
-    it "asks player to introduce position again without printing board" do
-      player = player1
-      column = "1"
-      expect(STDOUT).to receive(:puts)
-        .with("#{player.name}, introduce a column:")
+    let(:column) { "1" }
+
+    before do
+      allow(game).to receive(:puts)
+        .with("#{player1.name}, introduce a column:")
       allow(STDIN).to receive(:gets).and_return(column)
-      game.retry_turn(player)
+    end
+
+    it "asks player to introduce position again without printing board" do
+      game.retry_turn(player1)
+      expect(game).to have_received(:puts)
+        .with("#{player1.name}, introduce a column:")
     end
   end
 
@@ -46,21 +50,24 @@ describe Game do
 
     it "restarts game if 'y'" do
       allow(STDIN).to receive(:gets).and_return("y")
-      expect(game).to receive(:restart_game)
+      allow(game).to receive(:restart_game)
       game.try_again
+      expect(game).to have_received(:restart_game)
     end
 
     it "exits game if 'n'" do
       allow(STDIN).to receive(:gets).and_return("n")
-      expect(game).to receive(:exit_game)
+      allow(game).to receive(:exit_game)
       game.try_again
+      expect(game).to have_received(:exit_game)
     end
 
     it "asks to type 'y' or 'n'" do
       allow(STDIN).to receive(:gets).and_return("maybe")
-      expect(printer).to receive(:print_board)
-      expect(STDOUT).to receive(:puts).with("Please type 'y' or 'n'.")
+      allow(printer).to receive(:print_board)
+      allow(game).to receive(:puts).with("Please type 'y' or 'n'.")
       game.try_again
+      expect(game).to have_received(:puts).with("Please type 'y' or 'n'.")
     end
   end
 end
